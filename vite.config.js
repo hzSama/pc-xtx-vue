0785c0293e8318a0638a091ef66727ad56b1a00b
hzsama <979741259@qq.com>
2023-05-25 20:43:36 +0800

    element自定义主题色

--- vite.config.js
+++ vite.config.js
@@ -17,13 +17,22 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      // 指定使用sass作为样式配色系统
+      resolvers: [ElementPlusResolver({ importStyle: "sass" })],
     }),
   ],
   resolve: {
     // 实际的路径转化
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `@use "@/styles/element/index.scss" as *;`,
+      }
     }
   }
 })
