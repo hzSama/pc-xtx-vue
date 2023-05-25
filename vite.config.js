13ab733dca0ad27d7199b72829150b13f9e80d01
hzsama <979741259@qq.com>
2023-05-25 20:17:53 +0800

    别名联想配置及配置elementPlus

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 导入element-ui组件
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // 配置element-ui
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
+    // 实际的路径转化
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
