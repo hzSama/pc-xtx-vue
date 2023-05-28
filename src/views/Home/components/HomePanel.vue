26abadb959c544ebfc02a35a88b856e1a2c72aab
hzsama <979741259@qq.com>
2023-05-28 20:09:48 +0800

    封装home可复用面板组件

---
+++ src/views/Home/components/HomePanel.vue
@@ -0,0 +1,58 @@
+
+<!--把新鲜好物与热门推荐相同的部分抽出来封装成一个组件，抽象可变的部分通过prop或插槽传入。提高复用性。-->
+
+<script setup>
+// 定义props
+defineProps({
+  title: {
+    type: String
+  },
+  subTitle: {
+    type: String
+  }
+})
+
+// 定义
+</script >
+
+<template>
+  <div class="home-panel">
+    <div class="container">
+      <div class="head">
+        <!-- 主标题和副标题:使用prop -->
+        <h3>
+          {{ title }}<small>{{ subTitle }}</small>
+        </h3>
+      </div>
+      <!-- 主体内容区域:使用插槽 -->
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped lang='scss'>
+.home-panel {
+  background-color: #fff;
+
+  .head {
+    padding: 40px 0;
+    display: flex;
+    align-items: flex-end;
+
+    h3 {
+      flex: 1;
+      font-size: 32px;
+      font-weight: normal;
+      margin-left: 6px;
+      height: 35px;
+      line-height: 35px;
+
+      small {
+        font-size: 16px;
+        color: #999;
+        margin-left: 20px;
+      }
+    }
+  }
+}
+</style>
