519ff74963f3fe22def987e5eeff2b5f7f656c84
hzsama <979741259@qq.com>
2023-05-28 16:41:35 +0800

    吸顶导航的交互

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,9 +2,11 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from './components/LayoutFixed.vue'
 </script>
 
 <template>
+  <LayoutFixed></LayoutFixed>
   <LayoutNav></LayoutNav>
   <LayoutHeader></LayoutHeader>
   <!--二级路由出口-->
