8347a0b4108b9ad67f8e41dd8fa5c4e669c5859c
hzsama <979741259@qq.com>
2023-05-31 11:33:07 +0800

    axios请求拦截器中携带token

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,4 +1,5 @@
 // 创建一个axios实例
+import { useUserStore } from "@/stores/user"
 import axios from "axios"
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
@@ -9,7 +10,16 @@ const http = axios.create({
 })
 
 // axios请求拦截器
+/* 拦截器携带token：token是用户标识，有些数据需要携带token才允许请求，所以需要在接口调用时携带token。
+                   但为了统一控制采取请求拦截器携带的方案。 */
 http.interceptors.request.use(config => {
+  // 发送请求时若有token则将token放到请求头的Authorization属性中
+  const useStore = useUserStore()
+  const token = useStore.userData.token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
   return config
 }, e => Promise.reject(e))
 
