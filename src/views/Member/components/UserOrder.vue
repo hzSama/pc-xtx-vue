be581868be05d9a7806a2fefc1bd10a6333454c9
hzsama <979741259@qq.com>
2023-06-02 17:11:38 +0800

    实现我的订单页tab栏切换功能

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -23,11 +23,17 @@ const getOrderList = async () => {
   orderList.value = result.items
 }
 onMounted(() => getOrderList())
+
+// tab切换事件
+const tabChange = (type) => {
+  params.value.orderState = +type
+  getOrderList()
+}
 </script>
 
 <template>
   <div class="order-container">
-    <el-tabs>
+    <el-tabs @tab-change="tabChange">
       <!-- tab切换 -->
       <el-tab-pane v-for="item in tabTypes" :key="item.name" :label="item.label" />
 
